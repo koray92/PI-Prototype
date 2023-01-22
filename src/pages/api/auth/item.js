c34dc3205b55c6b0329368c41aaef39cb6f4a2ef
muratyvz13 <73107233+muratyvz13@users.noreply.github.com>
2023-01-23 02:12:13 +0300

    Updated contact informations

--- src/pages/api/auth/item.js
+++
@@ -1,7 +0,0 @@
-import { prisma } from "../../../../server/db/client";
-import product from "../../product";
-
-export default async function handler(req, res) {
-    const item = await prisma.item.findMany();
-    res.json(item);
-}
